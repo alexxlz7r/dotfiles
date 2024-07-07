340729bd519e5f18c9375f94206977bbcebec0d5
alexxlz7r <alexxlz7r@gmail.com>
2024-07-07 11:55:24 +0300

    feat: add install nerd fonts

---
+++ install_fonts.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo pamac install --no-confirm ttf-inconsolata-lgc-nerd ttf-anonymouspro-nerd ttf-bitstream-vera-mono-nerd ttf-dejavu-nerd ttf-firacode-nerd ttf-hack-nerd ttf-inconsolata-lgc-nerd ttf-jetbrains-mono-nerd ttf-liberation-mono-nerd ttf-meslo-nerd ttf-noto-nerd ttf-roboto-mono-nerd ttf-sourcecodepro-nerd ttf-terminus-nerd

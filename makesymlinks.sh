719bf35c74ee1d95526e90d31d17e2da9dd7b0c7
alexxlz7r <alexxlz7r@gmail.com>
2024-08-02 08:09:46 +0300

    feat: update install tools

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -27,18 +27,18 @@ echo "done"
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
     if [ -e ~/.$file ]; then
-	    if [ ! -L ~/.$file ]; then
-	        echo "Moving any existing dotfiles from ~ to $olddir"
-    	    mv ~/.$file $olddir
+        if [ ! -L ~/.$file ]; then
+            echo "Moving any existing dotfiles from ~ to $olddir"
+            mv ~/.$file $olddir
             echo "Creating symlink to $file in home directory."
             ln -s $dir/$file ~/.$file
-	    else 
-	        echo "File ~/.$file is a symlink" 
-	    fi
+        else
+            echo "File ~/.$file is a symlink"
+        fi
     else
-    	echo "File ~/.$file doesn't exist"
-	    echo "Creating symlink to $file in home directory."
-     	ln -s $dir/$file ~/.$file
+        echo "File ~/.$file doesn't exist"
+        echo "Creating symlink to $file in home directory."
+        ln -s $dir/$file ~/.$file
     fi
 done
 
@@ -49,48 +49,48 @@ done
 
 
 install_zsh () {
-# Test to see if zshell is installed.  If it is:
-if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
-    # Clone my oh-my-zsh repository from GitHub only if it isn't already present
-    if [[ ! -d $dir/oh-my-zsh/ ]]; then
-        git clone http://github.com/robbyrussell/oh-my-zsh.git
-    fi
-
-    if [[ ! -d $dir/oh-my-zsh/custom/plugins/zsh-autosuggestions ]]; then
-        git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-    fi
-
-    if [[ ! -d $dir/oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]]; then
-        git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-    fi
-
-    # Set the default shell to zsh if it isn't currently set to zsh
-    if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
-        chsh -s $(which zsh)
-    fi
-else
-    # If zsh isn't installed, get the platform of the current machine
-    platform=$(uname);
-    # If the platform is Linux, try an apt-get to install zsh and then recurse
-    if [[ $platform == 'Linux' ]]; then
-        if [[ -f /etc/redhat-release ]]; then
-            sudo yum install zsh
-            install_zsh
+    # Test to see if zshell is installed.  If it is:
+    if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
+        # Clone my oh-my-zsh repository from GitHub only if it isn't already present
+        if [[ ! -d $dir/oh-my-zsh/ ]]; then
+            git clone http://github.com/robbyrussell/oh-my-zsh.git
         fi
-        if [[ -f /etc/debian_version ]]; then
-            sudo apt-get install zsh
-            install_zsh
+        
+        if [[ ! -d $dir/oh-my-zsh/custom/plugins/zsh-autosuggestions ]]; then
+            git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
         fi
-	if [[ -f /etc/manjaro-release ]]; then
-	    sudo pamac install zsh
-	    install_zsh
-	fi
-    # If the platform is OS X, tell the user to install zsh :)
-    elif [[ $platform == 'Darwin' ]]; then
-        echo "Please install zsh, then re-run this script!"
-        exit
+        
+        if [[ ! -d $dir/oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]]; then
+            git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+        fi
+        
+        # Set the default shell to zsh if it isn't currently set to zsh
+        if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
+            chsh -s $(which zsh)
+        fi
+    else
+        # If zsh isn't installed, get the platform of the current machine
+        platform=$(uname);
+        # If the platform is Linux, try an apt-get to install zsh and then recurse
+        if [[ $platform == 'Linux' ]]; then
+            if [[ -f /etc/redhat-release ]]; then
+                sudo yum install zsh
+                install_zsh
+            fi
+            if [[ -f /etc/debian_version ]]; then
+                sudo apt-get install zsh
+                install_zsh
+            fi
+            if [[ -f /etc/manjaro-release ]]; then
+                sudo pamac install zsh
+                install_zsh
+            fi
+            # If the platform is OS X, tell the user to install zsh :)
+            elif [[ $platform == 'Darwin' ]]; then
+            echo "Please install zsh, then re-run this script!"
+            exit
+        fi
     fi
-fi
 }
 
 install_zsh
@@ -104,7 +104,15 @@ for tool in $cmd_tools; do
         echo "$tool is already installed"
     else
         echo "Installing $tool"
-        sudo pacman -Sy --noconfirm $tool
+        if [[ -f /etc/redhat-release ]]; then
+            sudo yum install $tool
+        fi
+        if [[ -f /etc/debian_version ]]; then
+            sudo apt-get install $tool
+        fi
+        if [[ -f /etc/manjaro-release ]]; then
+            sudo pacman -Sy --noconfirm $tool
+        fi
     fi
 done
 
